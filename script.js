3ac39c1952ebc32ff3efd16c4eb25bfd5d006bc4
Leandro Sciola <118067473+Leandro-Sciola@users.noreply.github.com>
2022-11-15 15:33:06 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+var $route = {"https://sciola-framework.github.io/#doc" : "https://api.github.com/repos/sciola-framework/documentation/contents/index.md"};
+
+function route(url) {
+    return ajax($route[url]);
+}
+
+function ajax(url) {
+    $.ajax({
+    url: url,
+      headers: {"Accept": "application/vnd.github.html"}
+    }).done(function(data) {
+      $("main").html('<div class="bg-white rounded w-100 m-0 p-4">' + data + '</div>');
+    });
+}
+
+function router(e) {
+    let url = $(location).attr("href");
+    if ($route[url]) {
+        return ajax($route[url]);
+    }
+}
+
+window.addEventListener("load", router);
+window.addEventListener("hashchange", router);
