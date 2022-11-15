ee5c3bb8bf457b6eea622fe7657c1e5fc3529f28
Leandro Sciola <118067473+Leandro-Sciola@users.noreply.github.com>
2022-11-15 17:21:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,9 @@ function ajax(url) {
 }
 
 function router(e) {
-    alert(window.location.hash);
-    let hash = window.location.hash;
+    let href = window.location.href;
+    let hash = href.split(window.location.origin)[1];
+    alert(hash);
     if (route[hash]) {
         $("main").html('<div class="text-center text-white bg-primary rounded fs-4 w-100 m-0 p-4">Loading...</div>');
         return ajax(route[hash]);
