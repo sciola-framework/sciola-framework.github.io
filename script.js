6cce0749ec2fd9df10252fa1958727a44420cfdd
Leandro Sciola <118067473+Leandro-Sciola@users.noreply.github.com>
2022-11-16 13:05:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function router(e) {
         e.preventDefault();
         return ajax(route[path]);
     }
-    $("main").html('<!DOCTYPE html><html><head><title>404 Not Found</title></head><body><h1 style="background: #ccc; padding: 25px">HTTP Status - 404 Not Found</h1><p>The requested URL was not found on this server.</p><hr><address>' + window.location.href + '</address></body></html>');
+    $(document).html('<!DOCTYPE html><html><head><title>404 Not Found</title></head><body><h1 style="background: #ccc; padding: 25px">HTTP Status - 404 Not Found</h1><p>The requested URL was not found on this server.</p><hr><address>' + window.location.href + '</address></body></html>');
 }
 
 window.addEventListener("load", router);
