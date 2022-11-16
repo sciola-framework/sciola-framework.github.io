2e365dd57cc9d42b300fef0626c608d8612aae3b
Leandro Sciola <118067473+Leandro-Sciola@users.noreply.github.com>
2022-11-16 11:30:31 -0300

    Update route.js

--- route.js
+++ route.js
@@ -1,4 +1,4 @@
 var route = {
   "/#home" : "https://api.github.com/repos/sciola-framework/documentation/contents/framework/home.md",
-  "/doc/#home" : "https://api.github.com/repos/sciola-framework/documentation/contents/framework/config.md"
+  "/#config" : "https://api.github.com/repos/sciola-framework/documentation/contents/framework/config.md"
 }
