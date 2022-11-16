5905a184c4acfdbc6dcfdf28c7c56b1d8aab15dd
Leandro Sciola <118067473+Leandro-Sciola@users.noreply.github.com>
2022-11-16 13:18:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function ajax(url) {
 }
 
 function router(e) {
-    let path = window.location.href.split(window.location.origin)[1];
+    let path = window.location.href.split(window.location.origin)[1].replace(/\/+$/, "");
     if (route[path]) {
         $("main").html('<div class="text-center text-white bg-primary rounded fs-5 m-4 p-4">Loading...</div>');
         e.preventDefault();
