255d6b572b87ecb9ac17bf769ff6d6a1c64122cd
Leandro Sciola <118067473+Leandro-Sciola@users.noreply.github.com>
2022-11-16 11:11:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ function ajax(url) {
       url: url,
       headers: {"Accept": "application/vnd.github.html"}
     }).done(function(data) {
-        $("main").html('<div class="bg-white rounded w-100 m-0 p-4">' + data + '</div>');
+        $("main").html('<div class="w-100 m-0 p-4">' + data + '</div>');
     });
 }
 
 function router(e) {
     let path  = window.location.href.split(window.location.origin)[1];
     if (route[path]) {
-        $("main").html('<div class="text-center text-white bg-primary rounded fs-5 w-100 m-0 p-4">Loading...</div>');
+        $("main").html('<div class="text-center text-white bg-primary rounded fs-5 w-100 m-4 p-4">Loading...</div>');
         e.preventDefault();
         return ajax(route[path]);
     }
