714e7d069fea2b15edd3e5869ef8da21ac656ab1
Leandro Sciola <118067473+Leandro-Sciola@users.noreply.github.com>
2022-11-17 13:17:29 -0300

    Update route.js

--- route.js
+++ route.js
@@ -1,5 +1,22 @@
 var route = {
   "/doc" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/index.html",
-  "/doc/#file-md" : "https://api.github.com/repos/sciola-framework/documentation/contents/framework/index.md",
-  "/doc/#config" : "https://api.github.com/repos/sciola-framework/documentation/contents/framework/config.md"
+  "/doc/#file-md" : "https://api.github.com/repos/sciola-framework/documentation/contents/framework/file.md",
+  "/doc/#requirements" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/requirements.html",
+  "/doc/#installation" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/installation.html",
+  "/doc/#config" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/config.html",
+  "/doc/#database" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/database.html",
+  "/doc/#languages" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/languages.html",
+  "/doc/#layers" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/layers.html",
+  "/doc/#libraries" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/libraries.html",
+  "/doc/#packages" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/packages.html",
+  "/doc/#public" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/public.html",
+  "/doc/#routes" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/routes.html",
+  "/doc/#tests" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/tests.html",
+  "/doc/#writable" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/writable.html",
+  "/doc/#ajax" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/ajax.html",
+  "/doc/#css" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/css.html",
+  "/doc/#js" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/js.html",
+  "/doc/#filters" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/filters.html",
+  "/doc/#functions" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/functions.html",
+  "/doc/#layout" : "https://raw.githubusercontent.com/sciola-framework/documentation/main/framework/layout.html"
 }
